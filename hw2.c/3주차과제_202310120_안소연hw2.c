51ab48c9172807d4035bfd11e64f26e95dcb3fc4
asy0108 <asy040108@naver.com>
2024-09-25 23:57:08 +0900

    Add C File[3주차 과제_202310120_안소연hw2.c]

---
+++ hw2.c/3주차과제_202310120_안소연hw2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(void)
+{
+	double kilometers;
+	printf("please enter kilometers: ");
+	scanf_s("%lf", &kilometers);
+
+	double miles = kilometers * 0.621371;
+	printf("%.1f km is equal to %.1f miles.\n", kilometers, miles);
+
+	return 0;
+	                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              
+
+
+}
