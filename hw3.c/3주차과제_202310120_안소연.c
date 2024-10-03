41227149672de7690c8ad18bdd5c691ec3e80360
asy0108 <asy040108@naver.com>
2024-10-03 11:49:49 +0900

    Add C File[4주차과제_202310120_안소연]

---
+++ hw3.c/3주차과제_202310120_안소연.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(void)
+{
+	int i = 0;
+	int k = 0;
+	
+	for (; i < 5; i++)
+	{
+		for (; k < i; k++)
+		
+			printf("**");
+			printf("*\n");
+		
+			
+	}
+		
+
+return 0;
+}
